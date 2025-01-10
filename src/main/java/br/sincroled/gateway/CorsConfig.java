45b097b3afe8809071717f102a0ddbe84e12eb71
leandrocodegit <lpoliveira.ti@gmail.com>
2025-01-10 01:53:49 -0300

    Salvando gateway

--- src/main/java/br/sincroled/gateway/CorsConfig.java
+++ src/main/java/br/sincroled/gateway/CorsConfig.java
@@ -9,8 +9,6 @@ import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
-import java.util.Collections;
-
 @Configuration
 public class CorsConfig implements WebFluxConfigurer {
 
@@ -23,20 +21,16 @@ public class CorsConfig implements WebFluxConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
-
     @Bean
     public CorsWebFilter  corsConfigurationSource() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.addAllowedHeader("*"); // Permitir todos os cabeçalhos
-        corsConfig.addAllowedMethod("*"); // Permitir todos os métodos
-        corsConfig.setAllowCredentials(false); // Não permitir credenciais (cookies, headers de autorização, etc.)
-
-        // Defina uma única origem permitida (exemplo: http://sincroled.com.br)
-        corsConfig.setAllowedOrigins(Collections.singletonList("http://sincroled.com.br")); // Permitir apenas uma origem específica
+        corsConfig.addAllowedOrigin("http://sincroled.com.br");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.addAllowedMethod("*");
+        corsConfig.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig); // Aplicar a configuração para todos os endpoints
-
+        source.registerCorsConfiguration("/**", corsConfig);
         return new CorsWebFilter(source);
     }
 }
