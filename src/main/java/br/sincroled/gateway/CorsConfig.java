a7aaea721bf08750e0abf2f2885ea17e45db37ff
leandrocodegit <lpoliveira.ti@gmail.com>
2025-01-10 02:06:57 -0300

    Salvando gateway

--- src/main/java/br/sincroled/gateway/CorsConfig.java
+++ src/main/java/br/sincroled/gateway/CorsConfig.java
@@ -3,11 +3,8 @@ package br.sincroled.gateway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig  {
