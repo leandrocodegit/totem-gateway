2868ed8c2ba76c7f8fe0d28b727e78a73d7e3794
leandrocodegit <lpoliveira.ti@gmail.com>
2025-01-09 22:42:41 -0300

    Salvando gateway

--- src/main/java/br/sincroled/gateway/Routes.java
+++ src/main/java/br/sincroled/gateway/Routes.java
@@ -17,21 +17,16 @@ public class Routes {
     @Bean
     public RouteLocator routesApi(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("api", r ->
-                        r.path("api/**")
-                                .filters(f -> f.filter(filter)
-                                        .rewritePath("api", "/totem"))
-                                .uri("http://totem:8081")
+                .route("/api/totem", r ->
+                        r.path("totem/**").uri("http://totem:8081/")
                 ).build();
     }
 
     @Bean
     public RouteLocator routesComando(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("comando", r ->
-                        r.path("comando/**")
-                                .filters(f -> f.filter(filter))
-                                .uri("http://comando:8082/")
+                .route("/api/comando", r ->
+                        r.path("comando/**").uri("http://comando:8082/")
                 ).build();
     }
 }
