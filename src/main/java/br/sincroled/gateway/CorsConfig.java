aa6a998368cb4273b5e8f19772d1adb0cf7ea2af
leandrocodegit <lpoliveira.ti@gmail.com>
2025-01-10 00:35:36 -0300

    Salvando gateway

--- src/main/java/br/sincroled/gateway/CorsConfig.java
+++ src/main/java/br/sincroled/gateway/CorsConfig.java
@@ -6,10 +6,21 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig  implements WebFluxConfigurer {
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Configura CORS para permitir a origem desejada
+        registry.addMapping("/**")
+                .allowedOrigins("http://sincroled.com.br")  // Origem permitida
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Métodos permitidos
+                .allowedHeaders("*")  // Permitir todos os cabeçalhos
+                .allowCredentials(true);  // Se for necessário enviar credenciais
+    }
     @Bean
     public CorsWebFilter  corsConfigurationSource() {
         CorsConfiguration corsConfig = new CorsConfiguration();
